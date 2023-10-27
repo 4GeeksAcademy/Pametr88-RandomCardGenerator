b04ade6869943e0ef0348cf19728b907f05114f5
Pamela <140130365+Pametr88@users.noreply.github.com>
2023-10-27 15:02:48 +0000

    primer ccambio

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let numero1 = number[Math.floor(Math.random() * number.length)];
+  document.querySelector(".cambio").innerHTML = numero1;
+
+  let figure = ["&hearts", "&diamond", "&clubs"];
+  let figura1 = figure[Math.floor(Math.random() * figure.length)];
+  document.querySelector(".figure").innerHTML = figura1;
 };
